a71b643898419c87ddc8dad2d2bbe4b4f688831e
Maksym Ostapenko <mak7.ostapenko@gmail.com>
2019-02-24 06:27:00 +0200

    Added computing target  feature.

--- main.py
+++ main.py
@@ -11,45 +11,52 @@ from utils import delete_nones, encode_ids
 from features import time_discretization, find_hexagon_id_of_pickup_coords, create_city_hexagons
 
 
-def main():
+def main(features = False):
     # TODO: insert data cleaning
-    frame = pd.read_csv("../data/clear_data.scv")
+    if features:
+        frame = pd.read_csv("./data/clear_data.scv")
 
-    # DATA PREPROCESSING
-    frame = delete_nones(frame)
-    frame = encode_ids(frame)
-    # delete canceled requests
-    frame = frame[(frame['canceled_by_client'] == 0) & (frame['canceled_by_driver'] == 0)]
+        # DATA PREPROCESSING
+        frame = delete_nones(frame)
+        frame = encode_ids(frame)
+        # delete canceled requests
+        frame = frame[(frame['canceled_by_client'] == 0) & (frame['canceled_by_driver'] == 0)]
 
-    # get hexagon frame
-    # TODO: pass params of city
-    hexagon_frame = create_city_hexagons()
+        # get hexagon frame
+        # TODO: pass params of city
+        hexagon_frame = create_city_hexagons()
 
-    # TODO: add city column
-    # FEATURE GENERATION
-    time_period_frame = time_discretization(frame=frame)
-    frame['week_day'], frame['weekend'], frame['month'], frame['year'] = time_period_frame['week_day'], \
-                                                                         time_period_frame['weekend'], \
-                                                                         time_period_frame['month'], \
-                                                                         time_period_frame['year']
-    frame['hexagon_id'] = find_hexagon_id_of_pickup_coords(hexagon_frame, frame)
-    frame= frame.dropna()
-    frame = frame.reset_index(drop=True)
+        # TODO: add city column
+        # FEATURE GENERATION
+        time_period_frame = time_discretization(frame=frame)
+        frame['week_day'], frame['weekend'], frame['month'], frame['year'] = time_period_frame['week_day'], \
+                                                                             time_period_frame['weekend'], \
+                                                                             time_period_frame['month'], \
+                                                                             time_period_frame['year']
+        frame['hexagon_id'] = find_hexagon_id_of_pickup_coords(hexagon_frame, frame)
+        frame= frame.dropna()
+        frame = frame.reset_index(drop=True)
 
-    features = frame.groupby(['week_day', 'month', 'year', 'hexagon_id']).count()
-    feature_frame = features.reset_index()
-    feature_frame = feature_frame[['week_day', 'month', 'year', 'hexagon_id', 'ride_id']]
-    feature_frame.columns = ['week_day', 'month', 'year', 'hexagon_id', 'num_in']
-    feature_frame['weekend'] = [True if weekday in (5, 6) else False for weekday in feature_frame.week_day.values]
-    feature_frame['ride_distance'] = frame.groupby(['week_day', 'month', 'year', 'hexagon_id']).sum()['ride_distance'].values
+        features = frame.groupby(['week_day', 'month', 'year', 'hexagon_id']).count()
+        feature_frame = features.reset_index()
+        feature_frame = feature_frame[['week_day', 'month', 'year', 'hexagon_id', 'ride_id']]
+        feature_frame.columns = ['week_day', 'month', 'year', 'hexagon_id', 'num_in']
+        feature_frame['weekend'] = [True if weekday in (5, 6) else False for weekday in feature_frame.week_day.values]
+        feature_frame['ride_distance'] = frame.groupby(['week_day', 'month', 'year', 'hexagon_id']).sum()['ride_distance'].values
+
+        feature_frame['load_coef'] = feature_frame.num_in / feature_frame.num_in.max()
+
+        # save feature frame
+        feature_frame.to_csv("./data/features.scv", index=False)
+    else:
+        feature_frame = pd.read_csv("./data/features.scv")
 
 
 
-
-    # SPLIT DATA
-    X_train, X_val, y_train, y_val = train_test_split(X, y, train_size=0.9, random_state=42)
-
-    # X_test = test_df
+    # # SPLIT DATA
+    # X_train, X_val, y_train, y_val = train_test_split(X, y, train_size=0.9, random_state=42)
+    #
+    # # X_test = test_df
 
 
 
