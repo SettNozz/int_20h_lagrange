eb6b1c8f71bd95931d31f4f802cc71405290837d
SettNozz <mak7.ostapenko@gmail.com>
2019-02-24 01:29:22 +0000

    add functions to pipeline

--- features.py
+++ features.py
@@ -38,3 +38,44 @@ def time_discretization(df):
 
     new_df = pd.DataFrame({'week_day': week_day, 'weekend': weekend, 'month': month, 'year': year})
     return new_df
+
+
+def create_hexagons(lat, lon): 
+    """
+    For Lviv, coordinates: 49.83826, 24.02324.
+    Function, for deviding city into regions, and save coordinates for this boundaries to DF.
+    Coordinates must be a city center from OpenStreetMap
+    Takes:
+        lat: float value of latitude
+        lon: float value of longitude
+    Return:
+        df with coordinates
+    """
+    h3_address = h3.geo_to_h3(lat, lon, 8)
+    hexagons = h3.k_ring_distances(h3_address, 6)
+    list_address = [hex for el in hexagons for hex in el]
+    latitudes1, latitudes2, latitudes3, latitudes4, latitudes5, latitudes6 = list(), list(), list(), list(), list(), list()
+    longitudes1, longitudes2, longitudes3, longitudes4, longitudes5, longitudes6 = list(), list(), list(), list(), list(), list()
+    hexagon_id = list()
+    for index, element in enumerate(list_address):
+        hex_boundary = h3.h3_to_geo_boundary(element)
+        hexagon_id.append(index + 1)
+        latitudes1.append(hex_boundary[0][0])
+        longitudes1.append(hex_boundary[0][1])
+        latitudes2.append(hex_boundary[1][0])
+        longitudes2.append(hex_boundary[1][1])
+        latitudes3.append(hex_boundary[2][0])
+        longitudes3.append(hex_boundary[2][1])
+        latitudes4.append(hex_boundary[3][0])
+        longitudes4.append(hex_boundary[3][1])
+        latitudes5.append(hex_boundary[4][0])
+        longitudes5.append(hex_boundary[4][1])
+        latitudes6.append(hex_boundary[5][0])
+        longitudes6.append(hex_boundary[5][1])
+    df_address = pd.DataFrame({"hexagon_id": hexagon_id, "latitude1": latitudes1, "latitude2":latitudes2, 
+                           "latitude3": latitudes3, "latitude4": latitudes4, "latitude5": latitudes5, 
+                           "latitude6": latitudes6, "longitude1": longitudes1, "longitude2": longitudes2, 
+                           "longitude3": longitudes3, "longitude4": longitudes4, "longitude5": longitudes5, 
+                           "longitude6": longitudes6})
+    df_address.to_csv("../data/data_boundaries.csv", index=False)
+    return df_address
